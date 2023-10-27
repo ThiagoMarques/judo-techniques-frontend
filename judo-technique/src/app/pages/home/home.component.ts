a25cb505485ec446e59d536fea787f7c178a55ed
Thiago Marques <thiagossmarques@gmail.com>
2023-10-27 18:51:41 -0300

    feat: insere card e lista de técnicas

--- judo-technique/src/app/pages/home/home.component.ts
+++ judo-technique/src/app/pages/home/home.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from 'src/app/components/card/card.component';
+import { ListComponent } from 'src/app/components/list/list.component';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, ListComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  videoUrl: SafeResourceUrl | undefined;
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    // const url = 'https://player.vimeo.com/video/698792378?h=acb9b5c20a'; 
+    // this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  onVideoUrlSelected(url: string) {
+    this.videoUrl = url;
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
