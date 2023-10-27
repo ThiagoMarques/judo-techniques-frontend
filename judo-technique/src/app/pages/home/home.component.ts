fa817d60cafbf983c61b76eaefe5fa4621c48645
Thiago Marques <thiagossmarques@gmail.com>
2023-10-27 18:14:54 -0300

    chore: insere tela home e componente card

--- judo-technique/src/app/pages/home/home.component.ts
+++ judo-technique/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from 'src/app/components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
