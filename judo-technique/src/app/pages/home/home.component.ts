2e7120f8584e79f8cf04d96de2a087b6335c439e
Thiago Marques <thiagossmarques@gmail.com>
2023-10-28 00:40:17 -0300

    chore: insere api para consumo

--- judo-technique/src/app/pages/home/home.component.ts
+++ judo-technique/src/app/pages/home/home.component.ts
@@ -1,24 +1,47 @@
+import { JudoTechnique } from './../../types/types';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { ListComponent } from 'src/app/components/list/list.component';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ApiService } from 'src/app/services/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CardComponent, ListComponent],
+  imports: [CommonModule, CardComponent, ListComponent, HttpClientModule],
+  providers: [ApiService],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   videoUrl: SafeResourceUrl | undefined;
+  judoThrows: JudoTechnique[] = [];
 
-  constructor(private sanitizer: DomSanitizer) {}
+  loading: boolean = false;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
-    // const url = 'https://player.vimeo.com/video/698792378?h=acb9b5c20a'; 
-    // this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    this.getThrows();
+  }
+
+  getThrows() {
+    this.loading = true;
+    this.apiService.getThrows().subscribe({
+      next: (data: JudoTechnique[]) => {
+        this.judoThrows = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      }
+    });
   }
 
   onVideoUrlSelected(url: string) {
